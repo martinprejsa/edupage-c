3c416c62fb7c6220d33e4290534bcdf6b0acab45
Martin Prejsa <martinprejsa@duck.com>
2023-12-18 19:11:15 +0100

    Updated docs created doxyfile

--- include/edupage/edupage.h
+++ include/edupage/edupage.h
@@ -14,20 +14,46 @@ enum EdupageError {
 
 typedef void (*edupage_error_callback) (enum EdupageError error, char const * const msg);
 
+/**
+ * @brief Sets the error handling callback.
+ */
 void edupage_set_error_callback(edupage_error_callback c);
 
-/* Creates a client and log it in */
+/**
+ * @brief Creates EdupageClient and logs it in. 
+ * Use @link edupage_set_error_callback @endlink to handle errors.
+ * @param username edupage username
+ * @param password edupage password
+ * @param server edupage server name
+ * @return struct EdupageClient* client handle
+ */
 struct EdupageClient * edupage_client_create(char const username[static 1], char const password[static 1], char const server[static 1]);
 
-/* Destroys a client */
+/**
+ * @brief Destroys the client and cleans up.
+ * 
+ * @param client client handle
+ */
 void edupage_client_destroy(struct EdupageClient * client);
 
-/* Retrieves the canteen
-Date format: YYYYMMdd
-*/
+/**
+ * @brief Retrieves edupage canteen.
+ * 
+ * @param client client handle
+ * @param date format YYYYMMdd
+ * @return struct EdupageCanteen 
+ */
 struct EdupageCanteen edupage_client_get_canteen(struct EdupageClient const client[static 1], char const * const date);
 
-/* Changes the order status*/
+/**
+ * @brief Changes order status for a specific day.
+ * Use @link edupage_set_error_callback @endlink to handle errors.
+ * @param client client handle
+ * @param status true for subscribe false for unsubscribe
+ * @param date the specific date, format: YYYYMMdd
+ * @return true if sucess
+ * @return false on error
+ */
 bool edupage_client_canteen_change_order_status(struct EdupageClient const client[static 1], bool const status, char const * const date);
 
 #endif // EDUPAGE_EDUPAGE_H
